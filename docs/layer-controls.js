e2886f10ec3800efa31d110817bcdcecfa6cd287
Colm Britton <colmjude@gmail.com>
2021-05-20 13:07:49 +0100

    Toggle layer control on and off, return active controls

--- docs/layer-controls.js
+++ docs/layer-controls.js
@@ -12,11 +12,14 @@ function LayerControls ($module) {
 }
 
 LayerControls.prototype.init = function (params) {
-  this.$controls = this.$module.querySelectorAll(this.layerControlSelector)
+  this.setupOptions(params)
+  // returns a node list so convert to array
+  var $controls = this.$module.querySelectorAll(this.layerControlSelector)
+  this.$controls = Array.prototype.slice.call($controls)
 
   var boundControlClickHandler = this.onControlClick.bind(this)
 
-  nodeListForEach(this.$controls, function ($control) {
+  this.$controls.forEach(function ($control) {
     $control.addEventListener('click', boundControlClickHandler, true)
   })
 
@@ -25,9 +28,36 @@ LayerControls.prototype.init = function (params) {
 
 LayerControls.prototype.onControlClick = function (e) {
   console.log("I've been clicked", e.target, this)
+  var $currentControl = e.target
+  this.toggleActive($currentControl)
+}
+
+LayerControls.prototype.toggleActive = function ($control) {
+  console.log($control)
+  if ($control.dataset.layerControlActive === 'true') {
+    $control.dataset.layerControlActive = 'false'
+    $control.classList.add(this.layerControlDeactivatedClass)
+    console.log('currently active so deactivating', this.layerControlDeactivatedClass)
+  } else {
+    $control.dataset.layerControlActive = 'true'
+    $control.classList.remove(this.layerControlDeactivatedClass)
+  }
+}
+
+LayerControls.prototype.activeLayers = function () {
+  return this.$controls.filter(($control) => $control.dataset.layerControlActive === 'true')
+}
+
+LayerControls.prototype.deactivatedLayers = function () {
+  return this.$controls.filter(($control) => $control.dataset.layerControlActive === 'false')
+}
+
+LayerControls.prototype.datasetType = function ($control) {
+  return $control.dataset.layerControl
 }
 
 LayerControls.prototype.setupOptions = function (params) {
   params = params || {}
   this.layerControlSelector = params.layerControlSelector || '[data-layer-control]'
+  this.layerControlDeactivatedClass = params.layerControlDeactivatedClass || 'deactivated-control'
 }
