32f42e9ea6ffbae0e9ef42c9bf32091e7b2d2bb9
Colm Britton <colmjude@gmail.com>
2021-05-25 15:53:06 +0100

    rewrite LayerControls to use URL Params

--- docs/layer-controls.js
+++ docs/layer-controls.js
@@ -21,12 +21,29 @@ LayerControls.prototype.init = function (params) {
   // returns a node list so convert to array
   var $controls = this.$module.querySelectorAll(this.layerControlSelector)
   this.$controls = Array.prototype.slice.call($controls)
+  this.datasetNames = this.$controls.map($control => $control.dataset.layerControl)
+
+  // listen for changes to URL
+  var boundSetControls = this.setControls.bind(this)
+  window.addEventListener('popstate', function (event) {
+    console.log("URL has changed - back button")
+    boundSetControls()
+  })
+
+  // initial set up of controls (default or urlParams)
+  const urlParams = (new URL(document.location)).searchParams
+  if (!urlParams.has('layer')) {
+    // if not set then use default checked controls
+    this.updateURL()
+  } else {
+    // use URL params if available
+    this.setControls()
+  }
 
   var boundControlChkbxChangeHandler = this.onControlChkbxChange.bind(this)
 
   this.$controls.forEach(function ($control) {
     console.log(this)
-    this.initialState($control)
     $control.addEventListener('change', boundControlChkbxChangeHandler, true)
   }, this)
 
@@ -36,49 +53,87 @@ LayerControls.prototype.init = function (params) {
 LayerControls.prototype.onControlChkbxChange = function (e) {
   console.log("I've been toggled", e.target, this)
 
-  var $currentControl = e.target.closest(this.layerControlSelector)
-  this.toggleActive($currentControl)
-  this.map._fetchSinceControlAction = false
-}
+  var $clickedControl = e.target.closest(this.layerControlSelector)
 
-LayerControls.prototype.toggleActive = function ($control) {
-  let enabling
-  if ($control.dataset.layerControlActive === 'true') {
-    $control.dataset.layerControlActive = 'false'
-    $control.classList.add(this.layerControlDeactivatedClass)
-    console.log('currently active so deactivating', this.datasetType($control))
-    enabling = false
-  } else {
-    $control.dataset.layerControlActive = 'true'
-    $control.classList.remove(this.layerControlDeactivatedClass)
-    enabling = true
-  }
+  const datasetName = this.datasetName($clickedControl)
+  console.log(datasetName)
+  this.updateURL()
+
   // run provided callback
+  const enabled = this.getCheckbox($clickedControl).checked
   if (this.toggleControlCallback && isFunction(this.toggleControlCallback)) {
-    this.toggleControlCallback(this.map, this.datasetType($control), enabling)
+    this.toggleControlCallback(this.map, this.datasetName($clickedControl), enabled)
   }
 }
 
-LayerControls.prototype.initialState = function ($control) {
+LayerControls.prototype.get = function (dataset) {
+  return this.$controls.filter($control => $control.dataset.layerControl === dataset)
+}
+
+LayerControls.prototype.enable = function ($control) {
+  console.log('enable', this.datasetName($control))
   const $chkbx = $control.querySelector('input[type="checkbox"]')
-  if ($control.dataset.layerControlActive === 'true') {
-    $chkbx.checked = true
-    $control.classList.remove(this.layerControlDeactivatedClass)
-  } else {
-    $chkbx.checked = false
-    $control.classList.add(this.layerControlDeactivatedClass)
+  $chkbx.checked = true
+  $control.dataset.layerControlActive = 'true'
+  $control.classList.remove(this.layerControlDeactivatedClass)
+}
+
+LayerControls.prototype.disable = function ($control) {
+  console.log('disable', this.datasetName($control))
+  const $chkbx = $control.querySelector('input[type="checkbox"]')
+  $chkbx.checked = false
+  $control.dataset.layerControlActive = 'false'
+  $control.classList.add(this.layerControlDeactivatedClass)
+}
+
+LayerControls.prototype.setControls = function () {
+  const urlParams = (new URL(document.location)).searchParams
+
+  if (urlParams.has('layer')) {
+    // get the names of the enabled and disabled layers
+    // only care about layers that exist
+    const enabledLayerNames = urlParams.getAll('layer').filter(name => this.datasetNames.indexOf(name) > -1)
+    console.log('Enable:', enabledLayerNames)
+
+    const datasetNamesClone = [].concat(this.datasetNames)
+    const disabledLayerNames = datasetNamesClone.filter(name => enabledLayerNames.indexOf(name) === -1)
+
+    // map the names to the controls
+    const toEnable = enabledLayerNames.map(name => this.get(name)[0])
+    const toDisable = disabledLayerNames.map(name => this.get(name)[0])
+    console.log(toEnable, toDisable)
+
+    // pass correct this arg
+    toEnable.forEach(this.enable, this)
+    toDisable.forEach(this.disable, this)
   }
 }
 
-LayerControls.prototype.activeLayers = function () {
-  return this.$controls.filter(($control) => $control.dataset.layerControlActive === 'true')
+LayerControls.prototype.updateURL = function () {
+  const urlParams = (new URL(document.location)).searchParams
+  const enabledLayers = this.enabledLayers().map($control => this.datasetName($control))
+
+  urlParams.delete('layer')
+  enabledLayers.forEach(name => urlParams.append('layer', name))
+  console.log(urlParams.toString())
+  const newURL = window.location.pathname + '?' + urlParams.toString() + window.location.hash
+  window.history.pushState({}, '', newURL)
+  this.setControls()
 }
 
-LayerControls.prototype.deactivatedLayers = function () {
-  return this.$controls.filter(($control) => $control.dataset.layerControlActive === 'false')
+LayerControls.prototype.getCheckbox = function ($control) {
+  return $control.querySelector('input[type="checkbox"]')
 }
 
-LayerControls.prototype.datasetType = function ($control) {
+LayerControls.prototype.enabledLayers = function () {
+  return this.$controls.filter($control => this.getCheckbox($control).checked)
+}
+
+LayerControls.prototype.disabledLayers = function () {
+  return this.$controls.filter($control => !this.getCheckbox($control).checked)
+}
+
+LayerControls.prototype.datasetName = function ($control) {
   return $control.dataset.layerControl
 }
 
